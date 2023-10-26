087728efdfec90e43e5befc6c6afac4cd16b35f2
Genrey <genreycristobal99@gmail.com>
2023-10-27 00:07:04 +0800

    Database.java
    - Creation ng logic
    - Antok na

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -1,11 +1,27 @@
 package Database;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Database {
+    // Variable declaration
+    String db_user = "root";
+    String db_password = "";
+
+    Connection connector;
+    Connector connectDB = new Connector();
+
+    public void insertData (String table, String value) throws SQLException {
+
+        connector = connectDB.connectDB();
+        String query = "INSERT INTO " + table;
+        // Iniisip ko pa yung logic kung paano maraming where clause or maraming data iinsert which is hindi possible pag string lang.
+        // Idea is to magcreate ng isa pang function na nagttake naman ng array
+    }
+
+    public void pullData (String table, String value) throws SQLException {
+
+
+    }
 
 
 }
