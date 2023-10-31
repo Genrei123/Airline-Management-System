a015f7bd469e0bc96660cae089fd680a9d1b3c47
Ervhyne(Catcher) <119420792+Ervhyne@users.noreply.github.com>
2023-10-31 23:25:23 +0800

    Dashboard v1.3:
    DESIGN UPDATE:
    - Menu bar slider design changed (YT Stylw)
    - Dashboard form added
    
    FUNCTIONS UPDATE:
    - Can change forms now when clicking respective buttons.

--- src/Dashboard/DashboardController.java
+++ src/Dashboard/DashboardController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.Pane;
@@ -26,21 +27,52 @@ public class DashboardController implements Initializable {
     private JFXButton dashboard_menu;
 
     @FXML
-    private AnchorPane overlayPane;
+    private AnchorPane centerPane;
 
     @FXML
-    private AnchorPane centerPane;
+    private AnchorPane sales_form;
+
+    @FXML
+    private AnchorPane ticketRecords_form;
+
+    @FXML
+    private AnchorPane flightRecords_form;
+
+    @FXML
+    private AnchorPane bookedFlights_form;
+
+    @FXML
+    private TableView<?> bookedFlights_table;
+
+    @FXML
+    private AnchorPane dashboard_form;
+
+    @FXML
+    private AnchorPane overlayPane;
 
     @FXML
     private AnchorPane dashboard_slider;
 
     @FXML
-    private JFXButton logout_btn;
-            
+    private JFXButton dashboardForm_btn;
+
     @FXML
-    private JFXTreeTableView<?> bookedFlight_table;
+    private JFXButton bookedForm_btn;
+
+    @FXML
+    private JFXButton flightForm_btn;
+
+    @FXML
+    private JFXButton ticketForm_btn;
+
+    @FXML
+    private JFXButton salesForm_btn;
+
+    @FXML
+    private JFXButton logout_btn;
 
     private boolean isMenuVisible = false;
+    private JFXButton currentSelectedButton;
 
     public void toggleAdminMenu() {
         if (isMenuVisible) {
@@ -52,7 +84,7 @@ public class DashboardController implements Initializable {
 
     private void showMenu() {
         TranslateTransition slide = new TranslateTransition();
-        slide.setDuration(Duration.seconds(0.4));
+        slide.setDuration(Duration.seconds(0.2));
         slide.setNode(dashboard_slider);
         slide.setToX(0);
         slide.play();
@@ -65,7 +97,7 @@ public class DashboardController implements Initializable {
 
     private void hideMenu() {
         TranslateTransition slide = new TranslateTransition();
-        slide.setDuration(Duration.seconds(0.4));
+        slide.setDuration(Duration.seconds(0.2));
         slide.setNode(dashboard_slider);
         slide.setToX(-dashboard_slider.getWidth());
         slide.play();
@@ -90,10 +122,30 @@ public class DashboardController implements Initializable {
         }
     }
 
+    private void switchForm(AnchorPane targetForm, JFXButton selectedButton) {
+        // Hide all forms
+        dashboard_form.setVisible(false);
+        bookedFlights_form.setVisible(false);
+        flightRecords_form.setVisible(false);
+        ticketRecords_form.setVisible(false);
+        sales_form.setVisible(false);
+
+        // Show the selected form
+        targetForm.setVisible(true);
+
+        // Update button styles
+        if (currentSelectedButton != null) {
+            currentSelectedButton.getStyleClass().remove("selected-button");
+        }
+
+        currentSelectedButton = selectedButton;
+        currentSelectedButton.getStyleClass().add("selected-button");
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Ensure the menu is initially closed
-        dashboard_slider.setTranslateX(-150);
+        dashboard_slider.setTranslateX(-180);
         overlayPane.setVisible(false);
 
         // Add event handlers to the overlayPane and topPane
@@ -108,5 +160,22 @@ public class DashboardController implements Initializable {
                 hideMenu();
             }
         });
+
+        // Set the dashboard_form as the default visible form
+        dashboard_form.setVisible(true);
+
+        // Apply the selected-button style to the dashboardForm_btn
+        dashboardForm_btn.getStyleClass().add("selected-button");
+
+        // Create a reference to the currently selected button
+        currentSelectedButton = dashboardForm_btn;
+
+        // Add button click event handlers
+        dashboardForm_btn.setOnAction(e -> switchForm(dashboard_form, dashboardForm_btn));
+        bookedForm_btn.setOnAction(e -> switchForm(bookedFlights_form, bookedForm_btn));
+        flightForm_btn.setOnAction(e -> switchForm(flightRecords_form, flightForm_btn));
+        ticketForm_btn.setOnAction(e -> switchForm(ticketRecords_form, ticketForm_btn));
+        salesForm_btn.setOnAction(e -> switchForm(sales_form, salesForm_btn));
+
     }
 }
