a2a7aae56011575fe4de1dd03c331dcd5143a375
Genrey <genreycristobal99@gmail.com>
2023-11-08 06:29:05 +0800

    Added Receipt maker

---
+++ src/Receipt/ReceiptMaker.java
@@ -0,0 +1,93 @@
+package Receipt;
+
+
+import com.itextpdf.kernel.colors.Color;
+import com.itextpdf.kernel.colors.DeviceRgb;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.borders.Border;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.LineSeparator;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import java.util.Date;
+
+public class ReceiptMaker {
+    public static void main(String[] args) throws FileNotFoundException {
+        // Directory and name of the file
+        String currentDir = System.getProperty("user.dir");
+        StringBuilder path = new StringBuilder();
+        String TICKET_NUMBER = "20220456";
+        path.append(currentDir + "/src/Receipt/" + TICKET_NUMBER + ".pdf");
+
+        // Properties of the said file
+        PdfWriter pdfWriter = new PdfWriter(path.toString());
+        PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+        pdfDocument.setDefaultPageSize(PageSize.A4);
+
+        // Writing starts here
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDate = date.format(formatter);
+
+        // Header
+        Document document = new Document(pdfDocument);
+        float twoColumnWidth[] = {285+150, 285};
+        Table header = new Table(twoColumnWidth);
+
+        Color headerColor = new DeviceRgb(0, 0, 255);
+        Color headerFontColor = new DeviceRgb(255, 255, 255);
+        //header.setBackgroundColor(headerColor);
+        //header.setFontColor(headerFontColor);
+
+        header.addCell(new Cell().add(new Paragraph("ERMINO AIRLINES").setFontSize(25).setBold()).setBorder(Border.NO_BORDER));
+        header.addCell(new Cell().add(new Paragraph("TICKET NUMBER: " + TICKET_NUMBER + "\n" + "DATE: " + formattedDate)).setBorder(Border.NO_BORDER));
+        document.add(header);
+
+        // Space
+        document.add(new Paragraph("\n"));
+
+        // Body
+        float twoColumnWidthBody[] = {285+150, 285};
+        Table passengerInfobody = new Table(twoColumnWidthBody);
+
+        // Passenger Info
+        document.add(new Paragraph("PASSENGER INFO").setBold());
+        passengerInfobody.addCell(new Cell().add(new Paragraph(
+                "PASSENGER NAME: " + "John Doe" + "\n"
+                + "SEX: M" + "\n")).setBorder(Border.NO_BORDER));
+        passengerInfobody.addCell(new Cell().add(new Paragraph(
+                "PASSENGER SURNAME: " + "Doe" + "\n"
+                + "AGE: 25" + "\n")).setBorder(Border.NO_BORDER));
+        document.add(passengerInfobody);
+
+        // Space
+        document.add(new Paragraph("\n"));
+
+        // Flight Info
+        Table flightInfobody = new Table(twoColumnWidthBody);
+        document.add(new Paragraph("FLIGHT INFO").setBold());
+        flightInfobody.addCell(new Cell().add(new Paragraph(
+                "PASSENGER NAME: " + "John Doe" + "\n"
+                + "FLIGHT DATE: " + "12/12/2021" + "\n"
+                + "FLIGHT FROM: " + "Istanbul" + "\n")).setBorder(Border.NO_BORDER));
+
+        flightInfobody.addCell(new Cell().add(new Paragraph(
+                "FLIGHT NUMBER: " + "ER-123" + "\n"
+                + "FLIGHT TIME: " + "12:00" + "\n"
+                + "FLIGHT TO: " + "Ankara" + "\n")).setBorder(Border.NO_BORDER));
+        document.add(flightInfobody);
+
+        // Space
+
+
+        document.close();
+    }
+}
