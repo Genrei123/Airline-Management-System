2931e0da8df48710ff4692a6615a0bb47831c4a3
Ervhyne(Catcher) <119420792+Ervhyne@users.noreply.github.com>
2023-11-11 12:18:15 +0800

    HOMEPAGE v1.2:
    DESIGN UPDATES:
    - Added Animating Star Background for Homepage
    
    Available Features:
    - Can now use full background

--- src/Homepage/HomepageController.java
+++ src/Homepage/HomepageController.java
@@ -6,19 +6,22 @@ package Homepage;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.animation.TranslateTransition;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
+import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.SequentialTransition;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -32,6 +35,9 @@ public class HomepageController implements Initializable {
 
     @FXML
     private AnchorPane centrePane;
+
+    @FXML
+    private Pane starsPane;
 
     @FXML
     private AnchorPane menuSlider;
@@ -65,31 +71,13 @@ public class HomepageController implements Initializable {
     private double defaultSliderWidth = 232;
     private double defaultSliderHeight = 43;
 
+    
+    //MENU BAR SLIDER FUNCTION AND ANIMATION
     public void toggleMenuSlider() {
         if (menuOpen) {
             closeMenuSlider();
         } else {
             openMenuSlider();
-        }
-    }
-    
-    public void logout() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/LogIn/LoginPage.fxml"));
-            Parent root = loader.load();
-            Scene scene = new Scene(root);
-
-            // Get the stage from the current button's scene
-            Stage stage = (Stage) menu_logout.getScene().getWindow();
-
-            // Set the minimum width and height for the LogIn
-            stage.setMinWidth(600);
-            stage.setMinHeight(233);
-
-            stage.setResizable(false); // Make it unresizable
-            stage.setScene(scene);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
@@ -116,11 +104,74 @@ public class HomepageController implements Initializable {
         menuOpen = false;
     }
 
-    
-    
+    //LOG OUT FUNCTION
+    public void logout() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/LogIn/LoginPage.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+
+            // Get the stage from the current button's scene
+            Stage stage = (Stage) menu_logout.getScene().getWindow();
+
+            // Set the minimum width and height for the LogIn
+            stage.setMinWidth(600);
+            stage.setMinHeight(233);
+
+            stage.setResizable(false); // Make it unresizable
+            stage.setScene(scene);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //FOR HOMEPAGE STAR BG ANIMATION
+    private void createTwinklingStars() {
+        int numStars = 100;
+        ParallelTransition parallelTransition = new ParallelTransition();
+
+        for (int i = 0; i < numStars; i++) {
+            Pane star = createStar();
+            starsPane.getChildren().add(star);
+
+            FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1), star);
+            fadeTransition.setFromValue(1.0);
+            fadeTransition.setToValue(0.0);
+            fadeTransition.setCycleCount(Animation.INDEFINITE);
+            fadeTransition.setAutoReverse(true);
+
+            // Add a random initial delay to each star
+            Duration initialDelay = Duration.seconds(Math.random() * 5);
+            fadeTransition.setDelay(initialDelay);
+
+            parallelTransition.getChildren().add(fadeTransition);
+        }
+
+        parallelTransition.play();
+    }
+
+    private Pane createStar() {
+        Pane star = new Pane();
+
+        // Set a fixed size for the star, adjust as needed
+        double starSize = new Random().nextInt(5) + 1;
+        star.setPrefSize(starSize, starSize);
+
+        star.setLayoutX(new Random().nextDouble() * starsPane.getPrefWidth());
+        star.setLayoutY(new Random().nextDouble() * starsPane.getPrefHeight());
+
+        // Set the background color and radius directly
+        star.setStyle("-fx-background-color: white; -fx-background-radius: 50%;");
+
+        star.setOpacity(1.0);
+
+        return star;
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // Initialize the menu slider in the closed state
         closeMenuSlider();
+        createTwinklingStars();
     }
 }
